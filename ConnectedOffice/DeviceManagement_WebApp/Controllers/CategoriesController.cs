3d4a50d57e91111f9455351927d2d35503370242
Hugovs2000 <hugovans2000@gmail.com>
2022-09-24 20:47:40 +0200

    Added Level 1 repository to categories

--- ConnectedOffice/DeviceManagement_WebApp/Controllers/CategoriesController.cs
+++ ConnectedOffice/DeviceManagement_WebApp/Controllers/CategoriesController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using DeviceManagement_WebApp.Data;
 using DeviceManagement_WebApp.Models;
+using DeviceManagement_WebApp.Repository;
 
 namespace DeviceManagement_WebApp.Controllers
 {
@@ -22,8 +23,13 @@ namespace DeviceManagement_WebApp.Controllers
         // GET: Categories
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Category.ToListAsync());
+            CategoryRepository categoryRepository = new CategoryRepository();
+
+            var results = categoryRepository.GetAll();
+
+            return View(results);
         }
+
 
         // GET: Categories/Details/5
         public async Task<IActionResult> Details(Guid? id)
